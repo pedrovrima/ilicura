9448ce9e01ac657e5c0f15dace420c392c1b3d42
Pedro Vítor Ribeiro Martins <pedrovrima@gmail.com>
2024-03-07 11:26:02 -0300

    cache should be working

--- next.config.js
+++ next.config.js
@@ -5,11 +5,6 @@
 await import("./src/env.js");
 import withPWAInit from "@ducanh2912/next-pwa";
 
-const speciesPages = Array.from({ length: 1897 }, (_, i) => ({
-  url: "/species/" + (i + 1),
-  revision: null,
-}));
-
 const withPWA = withPWAInit({
   dest: "public",
   cacheOnFrontEndNav: true,
