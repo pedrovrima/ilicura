0c54cfda6786dbfec810a453a90c5c2003394eee
Pedro Martins <pedrovrima@gmail.com>
2025-07-15 21:24:42 -0300

    fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+(async () => {
+  await import("./src/env.js");
+})();
 import withPWAInit from "@ducanh2912/next-pwa";
 import { ilicuraPageManifest, ilicuraPhotoManifest } from "./ilicura_data.js";
 
