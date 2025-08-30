ac8efa0bee9dc7d302c97445ed1e1631e4891e7d
Pedro Martins <pedrovrima@gmail.com>
2025-08-30 18:54:51 -0300

    sexual dimorphism new name

--- src/components/admin-mode/sexual-dimorphism/index.tsx
+++ src/components/admin-mode/sexual-dimorphism/index.tsx
@@ -18,7 +18,12 @@ export default function AdminSexualDimorphism({
   if (sexualDimorphism.data) {
     return (
       <div>
-        <h2 className="mb-8 text-xl font-bold">Dimorfismo Sexual</h2>
+        <h2 className="mb-8 text-xl font-bold">Informações de Idade e Sexo</h2>
+        <p className="mb-4 text-sm text-muted-foreground">
+          Selecione uma idade e se tem ou não dimorfismo sexual. Um novo campo
+          aparecerá para adicionar uma descrição da idade/sexo e adicionar
+          imagens.
+        </p>
         <div className="flex flex-col gap-4">
           <AddSexualDimorphism
             speciesId={speciesId}
